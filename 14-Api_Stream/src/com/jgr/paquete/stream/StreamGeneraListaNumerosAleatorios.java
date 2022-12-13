4f502c1cdeacc29a27088ea28407d4a7fbec05ba
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2022-12-13 07:30:11 +0100

    Tue Dec 13 07:30:10     20222022-12-10 HASTA EL 66,SEGUIR CON STREAM IV

---
+++ 14-Api_Stream/src/com/jgr/paquete/stream/StreamGeneraListaNumerosAleatorios.java
@@ -0,0 +1,28 @@
+package com.jgr.paquete.stream;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+
+/**
+ * The Class StreamGeneraListaNumerosAleatorios.
+ * Genera uyna lista de numeros aleatorios
+ */
+public class StreamGeneraListaNumerosAleatorios {
+
+	
+	public static void main(String[] args) {
+
+		List <Integer> lista =Stream.generate(()-> new Random()
+				.nextInt())
+				.limit(10)//10 elementos
+				.collect(Collectors.toList());
+	
+		lista.forEach(System.out::println);
+
+	}
+
+}
