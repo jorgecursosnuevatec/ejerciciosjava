43970afac0dd2e91dcd8a74e8b2d7980ca767511
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2022-12-17 10:51:55 +0100

    Sat Dec 17 10:45:58     20222022-12-17 APUESTA PRIMITIVA CON INTSTREAM

--- gitprune.sh
+++ gitprune.sh
@@ -14,7 +14,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-12-14 REPASAR REFERENCIAS A METODOS,repasado JDBC"
+message="$(date)2022-12-17 APUESTA PRIMITIVA CON INTSTREAM"
 fi
 
 start=`date +%s`
