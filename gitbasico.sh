0f3b1295318e6a5973e23be436e6bc423e2c9849
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2022-12-20 13:56:02 +0100

    Tue Dec 20 13:56:02     20222022-12-20 SUBIDA DESDE CASA

--- gitbasico.sh
+++ gitbasico.sh
@@ -21,7 +21,7 @@ inicio=`date +%s`
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-12-20 PRIMERA SUBIDA DESDE CASA"
+message="$(date)2022-12-20 SUBIDA DESDE CASA"
 fi
 
 git add .
