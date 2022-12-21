7a7629fb314dca1f88900d1eb0c442057c7f0d8e
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2022-12-21 15:22:46 +0100

    Wed Dec 21 15:22:46     20222022-12-21 SUBIDA FINAL casa

--- prueba/src/prueba/Clase1.java
+++
@@ -1,8 +0,0 @@
-package prueba;
-
-public class Clase1 {
-	public Clase1(int n) {
-		System.out.println("constructor parametro clase1");
-	}
-
-}
--- prueba/src/prueba/FiguraAbstracta.java
+++
@@ -1,9 +0,0 @@
-package prueba;
-
-public abstract class FiguraAbstracta {
-	
-	private float area;
-	
-	public  abstract float area();
-
-}
