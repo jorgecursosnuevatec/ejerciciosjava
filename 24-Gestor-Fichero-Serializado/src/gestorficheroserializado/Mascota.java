8b0bb12a4e8e0f026685188c270b415e5e530f71
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2022-12-27 14:47:00 +0100

    Tue Dec 27 14:41:37     20222022-12-26 seguir con el 75 preguntas examen

---
+++ 24-Gestor-Fichero-Serializado/src/gestorficheroserializado/Mascota.java
@@ -0,0 +1,12 @@
+package gestorficheroserializado;
+
+/**
+ * Clase que contiene el nombre de la mascota y el número de patas.
+ * @author Javier Abellán
+ *
+ */
+public class Mascota implements Serializable
+{
+    public String nombre;
+    public int numeroPatas;
+}
