c10e36e591f194e822704645f9c8bba213005a9f
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2023-01-16 09:16:21 +0100

    Mon Jan 16 09:16:21     20232023-01-16 SUBIDA DESDE CLASE

--- 02-Clase_Date_y_Calendar/src/com/jgr/fecha/y/calendario/JavaLocalDate.java
+++ 02-Clase_Date_y_Calendar/src/com/jgr/fecha/y/calendario/JavaLocalDate.java
@@ -146,24 +146,24 @@ public class JavaLocalDate {
 		esDespues = LocalDate.now().isAfter(LocalDate.now().minusDays(1));
 		System.out.println("esDespues = " + esDespues);
 
-		LocalDate ld1 = LocalDate.now();
-		LocalDate ld2 =LocalDate.of(2021, 7,22);
-		LocalTime t1=LocalTime.of(10,23,50); //10:23:50
-		LocalDateTime dt=LocalDateTime.of(2010,11,1,10,23,50); //2010-11-01T10:23:5
+		ld1 = LocalDate.now();
+		ld2 =LocalDate.of(2021, 7,22);
+		t1=LocalTime.of(10,23,50); //10:23:50
+		dt=LocalDateTime.of(2010,11,1,10,23,50); //2010-11-01T10:23:5
 		System.out.println("resultadoComparacion->"+ld1.isAfter(ld2));
 
 		System.out.println("suma 8 meses a la fecha"+ld1.plusMonths(8));
 
-		DateTimeFormatter format=DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		String fecha="20/09/2019";
-		LocalDate date=LocalDate.parse(fecha, format);
+		format=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		fecha="20/09/2019";
+		date=LocalDate.parse(fecha, format);
 
-		DateTimeFormatter format2=DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		LocalDate date2=LocalDate.of(2022,10,20);
+		format2=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		date2=LocalDate.of(2022,10,20);
 		//System.out.println(date2.format(format2)); //20/10/202
 
-		LocalDate f1=LocalDate.of(2019, 10, 30);
-		LocalDate f2=LocalDate.of(2016, 01, 31);
+		f1=LocalDate.of(2019, 10, 30);
+		f2=LocalDate.of(2016, 01, 31);
 		//comparación fechas
 		if(f1.isBefore(f2)) {
 			System.out.println("La fecha más antigua es: "+f1);
@@ -175,10 +175,10 @@ public class JavaLocalDate {
 		System.out.println(f1.plusMonths(8));
 		System.out.println(f1.plusDays(200));
 		System.out.println(f2.plusMonths(1));
-		DateTimeFormatter format1=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		format1=DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		System.out.println(f1.format(format1));
-		String mf="11/11/2018";
-		LocalDate nuevaFecha = LocalDate.parse(mf,format1);
+		mf="11/11/2018";
+		nuevaFecha = LocalDate.parse(mf,format1);
 		System.out.println("mf->"+nuevaFecha);
 
 		Date d1 = new Date();
