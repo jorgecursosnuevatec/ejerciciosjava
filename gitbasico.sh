ec1d37263f63b17cff9d575957476cd76ccaf289
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2023-01-17 13:09:41 +0100

    Tue Jan 17 13:09:40     20232023-01-17 SUBIDA DESDE CLASE

--- gitbasico.sh
+++ gitbasico.sh
@@ -21,7 +21,7 @@ inicio=`date +%s`
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2023-01-17 añadidos ejemplos de clase"
+message="$(date)2023-01-17 SUBIDA DESDE CLASE"
 fi
 
 git add .
