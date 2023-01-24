b4e66c858ed4e2f9b50b7688b9068d49a7e289a6
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2023-01-24 07:16:17 +0100

    Tue Jan 24 07:09:28     20232023-01-24 PROGRAMACION FUNCIONAL FLUJOS

--- gitprune.sh
+++ gitprune.sh
@@ -14,7 +14,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2023-01-23 PROGRAMACION FUNCIONAL FLUJOS"
+message="$(date)2023-01-24 PROGRAMACION FUNCIONAL FLUJOS"
 fi
 
 start=`date +%s`
